0ea2e886f88c1a13ee4fcdaf5e931ea498f17011
Juan Jose Fernandez Romero <piscis.ferro@gmail.com>
2015-10-19 16:15:02 +0200

    correcciones

---
+++ Accesos Directos para PHP.php
@@ -0,0 +1,12 @@
+<?php
+// Este archivo contiene distintqs lineas de codigo que pueden ser utiles 
+// Y tenerlas todas en un mismo lugar y copiar y pegar cuando sea necesario
+
+
+// Desactivar el boton submit  (cambiar enviar por la ID correspondiente)
+echo '<script>document.getElementById("enviar").disabled=true</script>';
+
+// Boton de recargar pagina
+echo '<button onclick ="document.location.href = document.location.href;">Recargar</button><br>';
+
+?>
