d03e04867feed114b38a729f3b181277bd8631cc
Juan Jose Fernandez Romero <piscis.ferro@gmail.com>
2015-10-20 00:28:41 +0200

    actu

--- Accesos Directos para PHP.php
+++ Accesos Directos para PHP.php
@@ -9,4 +9,14 @@ echo '<script>document.getElementById("enviar").disabled=true</script>';
 // Boton de recargar pagina
 echo '<button onclick ="document.location.href = document.location.href;">Recargar</button><br>';
 
+// Es una funcion que devuelve True or False dependiendo de si la palabra esta en el array
+in_array($variable, $array);
+
+//  Esta funcion para arrays asociativos devuelve la clave a partir de un valor 
+array_search($valor, $array);
+
+//  Esta funcion para arrays asociativos devuelve true o false dependiendo de si la variable
+// introducida existe como clave en el array
+array_key_exists($variable, $array)
+
 ?>
